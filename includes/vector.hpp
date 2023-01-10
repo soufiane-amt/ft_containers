17e5fdd352591a191ccdb8d255979eb7fccd6c77
Soufiane Amajat <samajat@e1r5p8.1337.ma>
2023-01-10 16:27:56 +0100

    setting bases up

---
+++ includes/vector.hpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   vector.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: samajat <samajat@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/09 17:05:29 by samajat           #+#    #+#             */
+/*   Updated: 2023/01/10 16:24:09 by samajat          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef VECTOR_HPP
+#define VECTOR_HPP
+
+#include <memory>
+/*ft::vector implementation*/
+
+namespace ft
+{
+    template <class T, class Allocator = std::allocator<T> >
+    class vector
+    {
+    public:
+        typedef T                                        value_type;
+        typedef Allocator                                allocator_type;
+        typedef value_type&                              reference;
+        typedef const value_type&                        const_reference;
+        typedef value_type*                              pointer;
+        typedef const value_type*                        const_pointer;
+        typedef size_t                                   size_type;
+    };
+        
+        
+}
+#endif
---
+++ tests/tst.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+#include <memory>
+#include <iostream>
+
+int main ()
+{
+     int a = 44245;
+    std::allocator<int>::reference ref = a;
+    std::cout << ref << std::endl;
+    // int *ptr = alloc.allocate(2);
+    // alloc.construct(ptr, 45);
+    // alloc.construct(ptr + 1, 100);
+    // std::cout << ptr[0] << std::endl;
+    // std::cout << ptr[1] << std::endl;
+    // alloc.deallocate(ptr, 1);
+}
