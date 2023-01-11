c2e656e67f4b9e5a83bb693c62b3258ac0dd120a
Soufiane Amajat <samajat@e1r4p14.1337.ma>
2023-01-11 17:30:32 +0100

    I'm still confused, it s like climbing a mountain for the first time, BS I never did the last one

--- includes/vector.hpp
+++ includes/vector.hpp
@@ -6,7 +6,7 @@
 /*   By: samajat <samajat@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/09 17:05:29 by samajat           #+#    #+#             */
-/*   Updated: 2023/01/10 16:24:09 by samajat          ###   ########.fr       */
+/*   Updated: 2023/01/11 17:28:30 by samajat          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,8 +29,34 @@ namespace ft
         typedef value_type*                              pointer;
         typedef const value_type*                        const_pointer;
         typedef size_t                                   size_type;
+        typedef typename allocator_type::size_type       size_type;
+        typedef typename allocator_type::difference_type difference_type;
+        typedef typename allocator_type::pointer         pointer;
+        typedef typename allocator_type::const_pointer   const_pointer;
+        /*Iterators
+                        */
+        
+        explicit vector (const allocator_type& alloc = allocator_type());
+        explicit vector (size_type n, const value_type& val = value_type(),  const allocator_type& alloc = allocator_type());
+        // template <class InputIterator>         vector (InputIterator first, InputIterator last,                 const allocator_type& alloc = allocator_type());
+        // vector (const vector& x);
     };
         
         
 }
+
+
+
+/*/////////////////////////////////////////////////////////////////
+                
+                    ft::vector Implementation
+
+/////////////////////////////////////////////////////////////////*/
+
+template <class T, class Allocator  >
+ft::vector <T, Allocator>::vector (const allocator_type& alloc)
+{
+    
+}
+
 #endif
