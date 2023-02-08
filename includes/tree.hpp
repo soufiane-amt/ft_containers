bc5505abf56daf7c4dabec5bade3005978cfdee7
Soufiane Amajat <samajat@e1r13p1.1337.ma>
2023-02-08 13:32:55 +0100

    this is fun

--- includes/tree.hpp
+++ includes/tree.hpp
@@ -6,7 +6,7 @@
 /*   By: samajat <samajat@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/07 15:34:18 by samajat           #+#    #+#             */
-/*   Updated: 2023/02/08 12:27:34 by samajat          ###   ########.fr       */
+/*   Updated: 2023/02/08 13:32:04 by samajat          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,13 @@ namespace ft
 {
 
 
-template <typename data>
+template <typename T>
 struct tree_node
 {
-    T           data;        
+    tree_node   *parent;
     tree_node   *left;
     tree_node   *right;
+    T           data;
 };
 
 
@@ -40,28 +41,30 @@ class binary_tree
     typedef     Key                                 key_type;
     typedef     T                                   mapped_type;
     typedef 	pair<const key_type,mapped_type>    value_type;
-    typedef 	data                    value_type;
-    typedef     Compare                 key_compare;
-    typedef     Allocator               allocator_type;
+    typedef 	data                                value_type;
+    typedef     Compare                             key_compare;
+    typedef     Allocator                           allocator_type;
     
 
-    binary_tree();
+    binary_tree(const   allocator_type&);
     binary_tree(const binary_tree& copy);
     binary_tree& operator=(const binary_tree& copy);
     ~binary_tree();
 
     private:
-    tree_node   *__tree_root;
-    
+    tree_node                                       *__tree_root;
+    allocator_type                                   __allocat;
     
     public:
-    tree_node   *create_node(value_type value);
 
     //insertion
     void         insert_node (tree_node *new_node);
-
     //searching
     tree_node   *search_node(key_type to_search);
+    
+    
+    private:
+    tree_node   *create_node(value_type value);
 };
 
 }
