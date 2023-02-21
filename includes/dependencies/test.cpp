4923f4d03e7cb2f2bbed98dd06e790bd19a0f668
Soufiane Amajat <samajat@e2r11p1.1337.ma>
2023-02-21 21:12:52 +0100

    fight

--- includes/dependencies/test.cpp
+++ includes/dependencies/test.cpp
@@ -6,7 +6,7 @@
 /*   By: samajat <samajat@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/21 16:55:57 by samajat           #+#    #+#             */
-/*   Updated: 2023/02/21 20:58:56 by samajat          ###   ########.fr       */
+/*   Updated: 2023/02/21 21:11:29 by samajat          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,27 +17,27 @@
 
 
 template <typename T>
-class comp
+class value_compare
 {
-private:
-    std::less<T> key_cmp;
     
-public:
-    
-    bool operator () (const T& a, const T& b)const
-    {
-        return key_cmp()(a < b);
-    }
+    protected:
+        std::less<T> comp;
+    public:
+        bool operator()(const T &_lhs, const T &_rhs) const
+        {
+            return comp(_lhs.first, _rhs.first);
+        }
 };
-typedef ft::binary_tree <int, int, comp<int>() ,std::allocator<int> > bst;
+
+typedef ft::binary_tree <int, int, value_compare<int> ,std::allocator<int> > bst;
 
 int main ()
 {
     bst a;
     
     a.insert_node(a.__tree_root, a.create_node( ft::pair <int, int> (4, 3)));
-    a.insert_node(a.__tree_root, a.create_node( ft::pair <int, int> (5, 3)));
-    a.insert_node(a.__tree_root, a.create_node( ft::pair <int, int> (1, 3)));
-    bst::iterator it = a.begin();
-    std::cout << it->first << std::endl;
+    // a.insert_node(a.__tree_root, a.create_node( ft::pair <int, int> (5, 3)));
+    // a.insert_node(a.__tree_root, a.create_node( ft::pair <int, int> (1, 3)));
+    // bst::iterator it = a.begin();
+    // std::cout << it->first << std::endl;
 }
