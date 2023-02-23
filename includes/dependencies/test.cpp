36d8c48bd4c1dc2d5c1cf44fadcb343d5483f273
Soufiane Amajat <samajat@e2r9p1.1337.ma>
2023-02-23 17:59:32 +0100

    testing things

--- includes/dependencies/test.cpp
+++ includes/dependencies/test.cpp
@@ -6,7 +6,7 @@
 /*   By: samajat <samajat@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/21 16:55:57 by samajat           #+#    #+#             */
-/*   Updated: 2023/02/23 17:55:27 by samajat          ###   ########.fr       */
+/*   Updated: 2023/02/23 17:59:07 by samajat          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,7 +40,7 @@ int main ()
 
 
     for (size_t i = 0; i < 10; i++)
-        a.insert_node(a.__tree_root, a.create_node(ft::make_pair(i, 3)), s);
+        a.insert(ft::make_pair(i, 3));
     // for (bst::iterator it = a.begin(); it != a.end(); it++)
     bst::iterator it = a.end();
     it--;
