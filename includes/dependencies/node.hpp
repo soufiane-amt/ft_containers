db8f9aca37be26c28823363636f8e519dd0a2506
Soufiane Amajat <samajat@e2r9p1.1337.ma>
2023-02-23 14:23:36 +0100

    good

--- includes/dependencies/node.hpp
+++ includes/dependencies/node.hpp
@@ -6,7 +6,7 @@
 /*   By: samajat <samajat@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/20 14:45:19 by samajat           #+#    #+#             */
-/*   Updated: 2023/02/23 14:18:21 by samajat          ###   ########.fr       */
+/*   Updated: 2023/02/23 14:23:18 by samajat          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -186,6 +186,16 @@ define_successor_grandPa_relation (Node<T>* to_delete, Node<T>* successor , Node
         grandPa->left = successor;
     else
         grandPa->left = successor;
+}
+
+template <class T>
+void 
+define_successor_bro_relation (Node<T>* to_delete, Node<T>* successor )
+{
+    Node<T>* broNdToSuccessor;
+
+    if (is_left_child(successor) && to_delete->)
+    broNdToSuccessor 
 }
 
 
