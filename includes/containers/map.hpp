6befa3e5af35c923da4ca5882561ea9c11bfb675
Soufiane Amajat <samajat@e1r4p14.1337.ma>
2023-03-02 11:19:53 +0100

    gooth

--- includes/containers/map.hpp
+++ includes/containers/map.hpp
@@ -6,7 +6,7 @@
 /*   By: samajat <samajat@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/06 16:13:44 by samajat           #+#    #+#             */
-/*   Updated: 2023/03/02 11:17:07 by samajat          ###   ########.fr       */
+/*   Updated: 2023/03/02 11:19:47 by samajat          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -230,9 +230,9 @@ bool operator!=( const map<Key, T, Compare, Alloc>& lhs,
 template< class Key, class T, class Compare, class Alloc >
 bool operator<( const map<Key, T, Compare, Alloc>& lhs,
                 const map<Key, T, Compare, Alloc>& rhs )
-                {
-                    
-                }
+{
+    return (std::lexicographical_compare(lhs.begin(), lhs.end(), rhs.begin()));
+}
                 
 template< class Key, class T, class Compare, class Alloc >
 bool operator<=( const map<Key, T, Compare, Alloc>& lhs,
