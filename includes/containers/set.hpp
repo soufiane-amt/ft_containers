d725930d4d0b4c004440d44972d60b489796e9ba
Soufiane Amajat <samajat@e2r11p1.1337.ma>
2023-03-03 18:35:32 +0100

    set is in process

---
+++ includes/containers/set.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   set.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: samajat <samajat@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/03 18:32:37 by samajat           #+#    #+#             */
+/*   Updated: 2023/03/03 18:34:58 by samajat          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SET_HPP
+#define SET_HPP
+#include <set>
+
+
+template < class T, class Compare = std::less<T>,  class Alloc = std::allocator<T> > 
+class set
+{
+    
+}
+
+
+#endif
